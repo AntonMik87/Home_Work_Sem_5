a461aff8fac0960e1b14b33de0212b496475142b
«Anton» <hranitel-liks@mail.ru>
2022-08-26 23:45:32 +0700

    HW003

--- Tasks/HW003/Program.cs
+++ Tasks/HW003/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://codeforces.com/contest/271/problem/A
+
+Console.Clear();
+
+int y = int.Parse(Console.ReadLine() ?? "0");
+int a, b, c, d;
+for (int z = y + 1; ; z++)
+{
+    a = z / 1000;
+    b = z / 100 % 10;
+    c = z / 10 % 10;
+    d = z % 10;
+    if (a != b && b != c && c != d && a != c && a != d && b != d)
+    {
+        Console.Write(z);
+        break;
+    }
+}
+
+
+

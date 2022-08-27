f1233e49cade0da1577462b1f34391b8d48a2fe4
«Anton» <hranitel-liks@mail.ru>
2022-08-27 21:02:28 +0700

    add HW

---
+++ Tasks/HW005/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://codeforces.com/contest/546/problem/A
+Console.WriteLine("Долларов за первый бана: ");
+int k = int.Parse(Console.ReadLine() ?? "0");    //k = 3
+Console.WriteLine("Сколько хочет купить бананов: ");
+int w = int.Parse(Console.ReadLine() ?? "0");  // w = 4
+Console.WriteLine("Введите количество долларов у солдата: ");
+int n = int.Parse(Console.ReadLine() ?? "0"); // n = 17
+int s = 0;
+s = k * (w * (w + 1) / 2);
+s = s - n;
+Console.Write($"Чтобы купить бананы, солдату не хватает: {s} долларов");
